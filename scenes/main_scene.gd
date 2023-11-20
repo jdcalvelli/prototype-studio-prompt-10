e9030b662c7f5786f76608e8bfd3221cca9b175c
JD Calvelli <jdcalvelli@JDs-MacBook-Pro.local>
2023-11-20 12:44:33 -0500

    adding beat signals and handling in scene w level array

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -3,11 +3,26 @@ extends Node2D
 var busIndex:int
 var recordEffect:AudioEffectRecord
 
-# Called when the node enters the scene tree for the first time.
+var levelArray = [false,true,false,true]
+var levelArrayIndex = 0
+
 func _ready():
 	busIndex = AudioServer.get_bus_index("MicIn")
+	
+	BeatManager.beatTimer.timeout.connect(
+		func():
+			# read the levelArray
+			if levelArray[levelArrayIndex]:
+				print("official beat")
+			
+			# loop back on array if we exceed size of it
+			if levelArrayIndex == levelArray.size() - 1:
+				levelArrayIndex = 0
+			# else move to next potential beat
+			else:
+				levelArrayIndex += 1
+	)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var audioSample:float = db_to_linear(AudioServer.get_bus_peak_volume_left_db(busIndex, 0))
 	if audioSample >= 0.8:
---
+++ scripts/beat_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var bpm:float = 90
+# beat duration = 60 / bpm
+var beatDuration:float = 60 / bpm
+
+var beatTimer:Timer = Timer.new()
+var eighthTimer:Timer = Timer.new()
+
+func _ready():
+	# create a timer for time beatduration
+	beatTimer.autostart = true
+	beatTimer.wait_time = beatDuration
+	add_child(beatTimer)
+	# create a timer for time beatduration / 2
+	eighthTimer.autostart = true
+	eighthTimer.wait_time = beatDuration / 2
+	add_child(eighthTimer)
